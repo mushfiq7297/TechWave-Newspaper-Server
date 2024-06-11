fa59fa3bcb54d08ff380ceb45c55ef44b8c0f0e8
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-06-11 15:19:10 +0600

    secure admin route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -27,8 +28,51 @@ async function run() {
 
     const newsCollection = client.db("techwaveDB").collection("news");
     const userCollection = client.db("techwaveDB").collection("users");
+
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+     // middlewares 
+     const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
      
 
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+
+
+    
     app.get("/allArticle", async (req, res) => {
       const result = await newsCollection.find().toArray();
       res.send(result);
@@ -36,7 +80,7 @@ async function run() {
 
 
     //user related api
-    app.get('/users', async (req, res) => {
+    app.get('/users',verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
